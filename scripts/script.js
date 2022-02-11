6c3547a687e090f280f9084fd5a09c080c87a283
Lucca Rauédys <luccarauedys@outlook.com>
2022-02-10 23:42:35 -0300

    enviar mensagem com clique e com enter

--- scripts/script.js
+++ scripts/script.js
@@ -39,36 +39,66 @@ function tentarNovamente(erro) {
 }
 
 function exibirMensagens(response) {
+  chat.innerHTML = "";
   let mensagensChat = response.data;
   let mensagem;
 
   for (let i = 0; i < mensagensChat.length; i++) {
-    //Teste pra ver o que aparece no console
-    console.log(mensagensChat[i]);
-
     let { from, to, text, type, time } = mensagensChat[i];
 
     if (type !== "status") {
       if (
         type === "message" ||
-        (type === "private_message" && to === username)
+        (type === "private_message" && to === username.name)
       ) {
-        mensagem = `<div class="message ${type}" data-identifier="message">
+        mensagem = `<div class="msg ${type}" data-identifier="message">
       <span>(${time})</span>
       <span> <strong>${from}</strong> para <strong>${to}:</strong> </span>
       <span>${text}</span>
       </div>`;
       }
     } else {
-      mensagem = `<div class="message status" data-identifier="message">
+      mensagem = `<div class="msg status" data-identifier="message">
       <span>(${time})</span>
       <span> <strong>${from}</strong> </span>
       <span>${text}</span>
       </div>`;
     }
     chat.innerHTML += mensagem;
-    document.querySelector("#chat div:last-child").scrollIntoView();
+    let ultimaMensagem = document.querySelector("#chat div:last-child");
+    if (ultimaMensagem != null) {
+      ultimaMensagem.scrollIntoView();
+    }
   }
 }
 
 setInterval(carregarMensagensServidor, 3000);
+
+function enviarMensagem() {
+  let input = document.getElementById("writeHere");
+  let mensagemDigitada = input.value;
+  let mensagemEnviada = {
+    from: username.name,
+    to: "Todos",
+    text: mensagemDigitada,
+    type: "message",
+  };
+  console.log(mensagemEnviada);
+  input.value = "";
+
+  let promise = axios.post(
+    "https://mock-api.driven.com.br/api/v4/uol/messages",
+    mensagemEnviada
+  );
+
+  promise.then(carregarMensagensServidor);
+  promise.catch(() => {
+    window.location.reload();
+  });
+}
+
+function enviarComEnter(e) {
+  if (e.key === "Enter") {
+    enviarMensagem();
+  }
+}
